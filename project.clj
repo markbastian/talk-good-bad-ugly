d783123481180ee0a4d612727eeaab083f90ae0d
Mark Bastian <mbastia@sandia.gov>
2015-03-16 09:25:58 -0600

    Changed project name.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject presentations "0.1.0-SNAPSHOT"
+(defproject awesome-clojure "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
